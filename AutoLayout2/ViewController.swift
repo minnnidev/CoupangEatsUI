cd58baa3510515c94dd46ba288b07a3fb9c58f97
Mini <minnnidev@gmail.com>
2022-07-13 23:27:42 +0900

    Initial Commit

---
+++ AutoLayout2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout2
+//
+//  Created by 김민 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
