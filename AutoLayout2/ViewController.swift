21b2587aa71213a1a38cc720cf9a05a1c87467f0
Mini <minnnidev@gmail.com>
2022-07-14 15:31:29 +0900

    CoupangEats Home Screen UI only AutoLayout

--- AutoLayout2/ViewController.swift
+++ AutoLayout2/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recommendationButton: UIButton!
+    @IBOutlet weak var deliveryButton: UIButton!
+    @IBOutlet weak var minDeliveryButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.buttonSetUp()
+    }
+    
+
+    func buttonSetUp() {
+        self.recommendationButton.layer.borderColor = UIColor.lightGray.cgColor
+        self.recommendationButton.layer.borderWidth = 1
+        self.recommendationButton.layer.cornerRadius = 15
+         
+        self.deliveryButton.layer.borderColor = UIColor.lightGray.cgColor
+        self.deliveryButton.layer.borderWidth = 1
+        self.deliveryButton.layer.cornerRadius = 15
+        
+        self.minDeliveryButton.layer.borderColor = UIColor.lightGray.cgColor
+        self.minDeliveryButton.layer.borderWidth = 1
+        self.minDeliveryButton.layer.cornerRadius = 15
     }
 
-
 }
-
